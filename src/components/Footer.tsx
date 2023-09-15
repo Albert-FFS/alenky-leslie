67a6cbecd7582233fe363ff8b966d66af5e87ffa
Albert-FFS <r.a.s.m.13@hotmail.com>
2023-09-14 19:15:48 -0700

    commit 09-14-2023 Estilizacion finalizado en elementos de texto. Continuar con media queries Terminar las vinculacion de sitios de los enlaces.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "../styles/Footer.css";
+import IconInstagram from "./icons/instagram.svg";
+import IconPatreon from "./icons/patreon.svg";
+import IconTwitter from "./icons/twitter-X.svg";
+import IconWebtoon from "./icons/webtoon.svg";
+import IconYoutube from "./icons/youtube.svg";
 export default function Footer() {
   return (
     <footer>
+      <div className="AboutMe">
+        <h4 >Acerca De Mi</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum ipsam
+          numquam repudiandae obcaecati accusamus nostrum. Eveniet
+          exercitationem, ullam quae error laboriosam mollitia laudantium
+          perspiciatis tenetur saepe ratione dignissimos inventore quas.
+        </p>
+      </div>
       <div className="LinksFooter">
         <a href="#" target="_blank" rel="noopener noreferrer">
           Acerca De Mi
@@ -27,7 +41,21 @@ export default function Footer() {
         </a>
       </div>
       <div className="SocialMedia">
-        
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src={IconInstagram} alt="Icon Instagram" />
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src={IconPatreon} alt="Icon Patreon" />
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src={IconTwitter} alt="Icon Twitter" />
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src={IconWebtoon} alt="Icon Webtoon" />
+        </a>
+        <a href="#" target="_blank" rel="noopener noreferrer">
+          <img src={IconYoutube} alt="Icon Youtube" />
+        </a>
       </div>
     </footer>
   );
