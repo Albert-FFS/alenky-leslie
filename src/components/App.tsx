f0552dd5c62a0c1511c0217fbc8f7c45bb0b7320
Albert-FFS <r.a.s.m.13@hotmail.com>
2023-09-17 17:14:05 -0700

    commit 09-17-2023 Agregado de 2 componentes nuevos Header Main

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,20 +2,15 @@ import React from "react";
 import SlideShow from "./SlideShow";
 import Navigator from "./Navigator";
 import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
 function App() {
   return (
     <>
       <SlideShow/>
       <Navigator/>
-
-      <header>
-        <h1>Bienvenido!</h1>
-        <h2>Soy Leslie Alenky</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, consequatur! Nihil totam qui voluptate debitis quidem hic tenetur nulla quia accusantium laborum? Dolore, repellat illum cum eius quidem tempora distinctio.</p>
-      </header>
-      <main>
-
-      </main>
+      <Header/>
+      <Main/>
       <Footer/>
     </>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Header.css";
+export default function Header() {
+  return (
+    <header>
+      <h1>Bienvenido!</h1>
+      <h2>Soy Leslie Alenky</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non,
+        consequatur! Nihil totam qui voluptate debitis quidem hic tenetur nulla
+        quia accusantium laborum? Dolore, repellat illum cum eius quidem tempora
+        distinctio.
+      </p>
+    </header>
+  );
+}
