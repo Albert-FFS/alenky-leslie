2a3e9e411d8649df982eec72981963faf81240c8
Albert-FFS <r.a.s.m.13@hotmail.com>
2023-09-17 19:28:39 -0700

    commit 09-17-2023 terminado de estructura de pagina
    Tareas a Hacer:
    Enviarselo al cliente
    Empezar a trabajar con React Router

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,145 @@
 import React from 'react'
-
+import "../styles/Main.css";
+import DemoImageSmall from "./img/Demo-Small-tyler lastovich.jpg";
+import DemoImageLarge from "./img/Demo-Full-tyler lastovich.jpg";
 export default function Main() {
   return (
-    <div>Main</div>
+    <main>
+      <section>
+        <h2>Arte Digital</h2>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+      </section>
+      <section>
+        <h2>Comics</h2>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+      </section>
+      <section>
+        <h2>Hula Dance</h2>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+      </section>
+      <section>
+        <h2>Modelaje</h2>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+      </section>
+      <section>
+        <h2>Store </h2>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+        <article>
+          <h3>Article</h3>
+          <img src={DemoImageSmall} alt="Image" />
+        </article>
+      </section>
+    </main>
   )
 }
